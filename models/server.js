568d13eb6c52352086a324bd506abfed2af6bfdd
anthony <anthony0813@gmail.com>
2023-04-15 09:16:52 -0500

    first version openaiv1.0

---
+++ models/server.js
@@ -0,0 +1,34 @@
+require("dotenv/config");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+class Server {
+  constructor() {
+    this.PORT = 5050;
+    this.app = express();
+    this.paths = {
+      api: "/api",
+    };
+    this.middleware();
+    this.router();
+  }
+
+  middleware() {
+    this.app.use(cors());
+    this.app.use(morgan("dev"));
+    this.app.use(express.json());
+  }
+
+  router() {
+    this.app.use(this.paths.api, require("../routes/openai"));
+  }
+
+  listen() {
+    this.app.listen(this.PORT, () => {
+      console.log(`the application listening on ${this.PORT}`);
+    });
+  }
+}
+
+module.exports = Server;
---
+++ routes/openai.js
@@ -0,0 +1,25 @@
+const { Router } = require("express");
+const { Configuration, OpenAIApi } = require("openai");
+
+const router = Router();
+
+router.get("/", async (req, res) => {
+  const { question } = req.body;
+
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: question }],
+  });
+
+  console.log(completion.data.choices[0].message);
+
+  res.json({
+    data: completion.data.choices[0].message.content,
+  });
+});
+
+module.exports = router;
